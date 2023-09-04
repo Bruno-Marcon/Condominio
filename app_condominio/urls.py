70b78e4ad928a5a95bd5de0bd4f27c085bf795eb
Bruno Marcon <55155016+Bruno-Marcon@users.noreply.github.com>
2023-09-04 01:27:08 -0300

    registration

--- app_condominio/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import home, save
-from django.conf.urls.static import static
-
-urlpatterns = [
-    path('', home),
-    path('save/', save, name="save"),
-]
--- condominio/urls.py
+++ condominio/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('app_condominio/', include('app_condominio.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
